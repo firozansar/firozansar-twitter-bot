475f5ed0ed30dbf55d685e417c6f80f85fa612ee
firozansar <firozansar@gmail.com>
2017-02-05 10:19:53 +0000

    Deploy to heroku

--- src/config.js
+++ src/config.js
@@ -7,17 +7,11 @@
 
 // Uncomment for when deploying to Heroku
 
-// module.exports = {
-//   consumer_key: process.env.CONSUMER_KEY,
-//   consumer_secret: process.env.CONSUMER_SECRET,
-//   access_token: process.env.ACCESS_TOKEN,
-//   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-// };
+ module.exports = {
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+};
 
-module.exports = {
-  consumer_key: '5tQjcXNtzW0EwNIyA1w8Fk4Ek',
-  consumer_secret: 'RHaKyfhDygrfzJonFg6jnBUhhOJgBY9DBAjNdAXDuJHHyYTaWj',
-  access_token: '80818804-8jduMbi6CTuUUjtPm5gMenmZ7BNCOchZ2LW1Snlfp',
-  access_token_secret: '3r1Rl8bhGXSi3MeiW5ujbtEdPAtY4sI9qOrW5z5f0tb2t'
-}
 
