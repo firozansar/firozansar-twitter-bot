9135d6d2f568df05c9c97a15be5bf1079454de0f
firozansar <firozansar@gmail.com>
2017-02-05 10:42:38 +0000

    config changes

--- src/config.js
+++ src/config.js
@@ -8,10 +8,9 @@
 // Uncomment for when deploying to Heroku
 
  module.exports = {
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret,
 };
-
 
