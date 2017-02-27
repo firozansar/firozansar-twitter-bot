69e0c9729b65a30ba48dd0f6471575d9141ceac7
firozansar <firozansar@gmail.com>
2017-02-27 19:08:10 +0000

    reply to followers modified

--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -17,13 +17,13 @@ module.exports = {
     'popular'
   ],
   responseString: [
-    'Hi @screenName thanks for the follow! !CR',
-    '@screenName thanks for following! !CR',
-    'Hey @screenName thanks for the follow! !CR',
-    'Thanks for following @screenName! !CR',
-    'Hey @screenName, thanks for following! !CR',
-    'Awesome @screenName, thanks for following! !CR',
-    'Thanks for the follow @screenName! !CR',
-    'Thanks for following @screenName! How are you today? !CR'
+    'Hi @screenName thanks for the follow!',
+    '@screenName thanks for following!',
+    'Hey @screenName thanks for the follow!',
+    'Thanks for following @screenName!',
+    'Hey @screenName, thanks for following!',
+    'Awesome @screenName, thanks for following!',
+    'Thanks for the follow @screenName!',
+    'Thanks for following @screenName! Have a nice day!'
   ]
 }
